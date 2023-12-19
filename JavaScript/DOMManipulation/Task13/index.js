82dac399e84525f0dfcac149189aadbf33d640d8
Rahul106 <therahulgupta106>
2023-12-20 00:39:04 +0530

    second commit

--- JavaScript/DOMManipulation/Task13/index.js
+++ JavaScript/DOMManipulation/Task13/index.js
@@ -1,35 +1,53 @@
 
 
+let subButton = document.getElementById('subBtn');
+
+subButton.addEventListener("click", function (event){
+
+    let name = document.getElementById('username').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+
+    let object = {
+        name, email, phone
+    };
+
+    let jsonstring = JSON.stringify(object);
+    localStorage.setItem(object.email, `${jsonstring}`);
+
+});
+
+
 // Write your code below:
-function handleFormSubmit(event) {
+// function handleFormSubmit(event) {
     
-    event.preventDefault();
+//     event.preventDefault();
    
-    var name = document.getElementById('username').value;
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
+//     var name = document.getElementById('username').value;
+//     var email = document.getElementById('email').value;
+//     var phone = document.getElementById('phone').value;
 
-    // Check if name and email are not empty
-    if (name && email && phone) {
-            // Get existing users from local storage or create an empty object
-        var users = JSON.parse(localStorage.getItem('users')) || {};
+//     // Check if name and email are not empty
+//     if (name && email && phone) {
+//             // Get existing users from local storage or create an empty object
+//         var users = JSON.parse(localStorage.getItem('users')) || {};
 
-        // Create a unique key for the user
-       var userId = email;
+//         // Create a unique key for the user
+//        var userId = email;
 
-            // Create user object
-        var user = {
-            name: name,
-            email: email,
-            phone: phone
-        };
+//             // Create user object
+//         var user = {
+//             name: name,
+//             email: email,
+//             phone: phone
+//         };
 
-        let myObj_serialized = JSON.stringify(user);
-        // Save the updated users object to local storage
-        localStorage.setItem(email, JSON.stringify(user));
-    }
+//         let myObj_serialized = JSON.stringify(user);
+//         // Save the updated users object to local storage
+//         localStorage.setItem(email, JSON.stringify(user));
+//     }
 
-}
+// }
 
 
 
