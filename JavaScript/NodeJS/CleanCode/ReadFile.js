3165d109bd20ebb3fcc80255854177182af6f019
Rahul106 <therahulgupta106>
2024-02-06 20:50:32 +0530

    initial commit
    initial commit
    This is the basic code for :
    1) post a data on url it will create a new file
    2) current data will appear and visible above input box
    3) It will read data from file and make it appear on appear and visible above input box
    4) This is the clean code with seperate files.

---
+++ JavaScript/NodeJS/CleanCode/ReadFile.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const routes = require('./routes');
+
+//const server = http.createServer(routes);
+const server = http.createServer(routes.handler);
+
+server.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});
